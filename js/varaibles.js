99d6274c4c327be9286bb03fb46123c9b3b5594b
Ethan Wright <ethan.wright@macfpm0h4lfd9.servicenow.com>
2024-12-05 09:26:12 -0800

    variable prac

---
+++ js/varaibles.js
@@ -0,0 +1,12 @@
+var x = 10;
+let y = 20;
+const z = 30;
+
+x = 15; // Try this
+y = 25; // Try this
+// z = 35; // What happens? it is a constant so you cannot reassign. 
+
+
+console.log(x);
+console.log(y);
+console.log(z);
