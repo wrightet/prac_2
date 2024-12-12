0eb34560d94b111ebaf3e1fdac2af470f0aee129
Ethan Wright <ethan.wright@macfpm0h4lfd9.servicenow.com>
2024-12-12 15:24:21 -0800

    string reverse start

---
+++ js/reverseString.js
@@ -0,0 +1,9 @@
+function reverseString(str) {
+    // Your code here
+  }
+
+console.log(reverseString("hello")); // "olleh"
+console.log(reverseString("JavaScript")); // "tpircSavaJ"
+console.log(reverseString("")); // ""
+console.log(reverseString("a")); // "a"
+console.log(reverseString("racecar")); // "racecar"
